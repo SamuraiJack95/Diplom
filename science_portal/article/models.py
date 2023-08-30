a9f9c234aaa72f981e59857aa0d47af4d527621b
Andrey <samurai_jack2@mail.ru>
2023-08-30 16:21:18 +0400

    add project style and templates

--- science_portal/article/models.py
+++ science_portal/article/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Article(models.Model):
 
     title = models.CharField(max_length=200)
+    authors = models.CharField(max_length=500)
     description = models.TextField(null=True, blank=True)
     featured_image = models.ImageField(null=True, blank=True, upload_to="article/%Y/%m/%d")
     tags = models.ManyToManyField('Tag', blank=True)
